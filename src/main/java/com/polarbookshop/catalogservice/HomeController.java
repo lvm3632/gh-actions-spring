618630d24418aec907bca1eed9d2129b3718c332
lvm3632 <lvm3632@gmail.com>
2025-02-08 15:46:54 -0600

    Add Actuator and config server

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,23 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@ConditionalOnProperty(name = "polar.testdata.enabled", havingValue = "true")
 public class HomeController {
+
+	private final PolarProperties polarProperties;
+
+	public HomeController(PolarProperties polarProperties) {
+		this.polarProperties = polarProperties;
+	}
 
 	@GetMapping("/")
 	public String getGreeting() {
-		return "Welcome to the book catalog!";
+		return polarProperties.getGreeting();
 	}
 
 }
